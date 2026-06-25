36174e99c30b8f08e8d38eed7f77649bcafa0e9b
Ali Waleed <aw3043622@gmail.com>
2026-06-25 14:44:46 +0300

    edit

--- Client/src/components/auth/LogoutButton.tsx
+++ Client/src/components/auth/LogoutButton.tsx
@@ -3,6 +3,7 @@ import { LogOut } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import LogoutModal from "./LogoutModal";
+import { useTranslation } from "react-i18next";
 
 const LogoutButton = () => {
   const navigate = useNavigate();
@@ -20,7 +21,7 @@ const LogoutButton = () => {
       setLoading(false); 
     }, 1000);
   };
-
+  const { t } = useTranslation();
   return (
     <>
       <button
@@ -28,7 +29,7 @@ const LogoutButton = () => {
         className="flex items-center gap-2 text-red-500 hover:bg-red-500/10 hover:text-red-600 px-3 py-2 rounded-md transition cursor-pointer focus:outline-none w-full "
       >
         <LogOut size={18} />
-        <span className="truncate">Log Out</span>
+        <span className="truncate">{t('sidebar.items.logout')}</span>
       </button>
 
       <LogoutModal
