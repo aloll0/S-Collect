7b6b1987fc7d4b67590fccb0805df0814b8ef043
zeyad810 <a.zeyad7100@gmail.com>
2026-07-21 16:31:42 +0300

    feat: implement Dashboard page with loading skeleton state

--- Client/src/pages/Dashboard.tsx
+++ Client/src/pages/Dashboard.tsx
@@ -4,18 +4,15 @@ import SalesChart from '../features/dashboard/SalesChart';
 import InventoryAlert from '../features/dashboard/InventoryAlert';
 import TopSelling from '../features/dashboard/TopSelling';
 import RecentOrdersTable from '../features/dashboard/RecentOrdersTable';
+import DashboardSkeleton from '../features/dashboard/skeleton/DashboardSkeleton';
 import { useProducts } from '../features/AddProducts/useProducts';
 
 const Dashboard = () => {
   const { t } = useTranslation();
-  const { data, isLoading, error } = useProducts();
-  if (isLoading) {
-    return <div>Loading...</div>;
+  const { data, isLoading } = useProducts();
+  if (isLoading || !data) {
+    return <DashboardSkeleton />;
   }
-  if (!data) {
-    return <div>{error ? error.message : 'Loading...'}</div>;
-  }
-  console.log(data)
   return (
     <div className="flex flex-col flex-1">
       <div className="sidebar-page-container flex items-center justify-between mb-10 bg-gray-50">
